3e08ff4418751e8bdb0b96be2f1c7eb0a6648926
hyunfunfun(gram) <hyunfunfun@naver.com>
2023-12-07 19:16:37 +0900

    Update hw_04_19_2020184009.py

--- hw_04_19_2020184009.py
+++ hw_04_19_2020184009.py
@@ -53,23 +53,23 @@ for k in range(n_clusters):
 
 print(dict(D))
 
-clusters = [ [] for _ in range(n_clusters) ]
-# n_clusters 개수만큼 빈 배열을 준비한다
-for i in range(n_pts) # 모든 점들에 대하여
-    # 정점 i 가 몇번째 center 와 가까운지 알아내자
-    ci = ???
-    clusters[ci].append(???)
-
-print(clusters)
-
-from welzl import welzl
-# 제일 큰 원의 반지름을 max_r 에 저장한다
-max_r = 0
-for ci in range(n_clusters):
-    x, y, r = welzl(clusters[ci]) # 반지름 r 을 구한다
-    print(f'{ci} - {len(clusters[ci]):2d} points: ({x:.1f}, {y:.1f}) r: {r:.1f}')
-    ....
-
-print(f'Max R = {max_r:.1f}')
-# Max R =357.6
+# clusters = [ [] for _ in range(n_clusters) ]
+# # n_clusters 개수만큼 빈 배열을 준비한다
+# for i in range(n_pts) # 모든 점들에 대하여
+#     # 정점 i 가 몇번째 center 와 가까운지 알아내자
+#     ci = ???
+#     clusters[ci].append(???)
+#
+# print(clusters)
+#
+# from welzl import welzl
+# # 제일 큰 원의 반지름을 max_r 에 저장한다
+# max_r = 0
+# for ci in range(n_clusters):
+#     x, y, r = welzl(clusters[ci]) # 반지름 r 을 구한다
+#     print(f'{ci} - {len(clusters[ci]):2d} points: ({x:.1f}, {y:.1f}) r: {r:.1f}')
+#     ....
+#
+# print(f'Max R = {max_r:.1f}')
+# # Max R =357.6
 
